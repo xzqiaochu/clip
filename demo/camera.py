3882b99a76b31bcedf23fd6f8c43b230c84b811e
xzqiaochu <xzzhangqiaochu@163.com>
2023-03-04 17:24:09 +0800

    update

--- demo/camera.py
+++ demo/camera.py
@@ -1,26 +1,19 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
 
 while True:
-
-    # 获取一帧视频
+    t = time.time()
     ret, frame = cap.read()
-
-    # 如果成功获取视频帧
     if ret:
-        # 进行处理
-        # ...
-
-        # 在窗口中实时显示视频帧
         cv2.imshow('Video', frame)
+        print(f"\r[{1/(time.time()-t):2.0f}fps]", end="")
+        t = time.time()
 
     # 等待用户按下ESC键退出
     if cv2.waitKey(1) == 27:
         break
 
-# 释放摄像头资源
 cap.release()
-
-# 关闭所有窗口
 cv2.destroyAllWindows()
